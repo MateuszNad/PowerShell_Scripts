177d47fae5e375365d9b157cd59befce03c7f002
mnadobnik <mnadobnik+github@gmail.com>
2020-12-16 20:38:58 +0100

    pierwsza proba

--- Microsoft.PowerShell.Crescendo/docker.psm1
+++ Microsoft.PowerShell.Crescendo/docker.psm1
@@ -1,66 +1,101 @@
 # Module created by Microsoft.PowerShell.Crescendo
 Function Get-DockerContainer
 {
-[CmdletBinding()]
+    [CmdletBinding()]
 
-param(
-[Parameter()]
-[switch]$Force,
-[Parameter()]
-[string]$Filter
+    param(
+        [Parameter()]
+        [switch]$Force,
+        [Parameter()]
+        [string]$Filter
     )
 
-BEGIN {
-    $__PARAMETERMAP = @{
-        Force = @{ OriginalName = '--all'; OriginalPosition = '0'; Position = '2147483647'; ParameterType = [switch]; NoGap = $False }
-        Filter = @{ OriginalName = '--filter'; OriginalPosition = '0'; Position = '2147483647'; ParameterType = [string]; NoGap = $False }
-    }
+    BEGIN
+    {
+        $__PARAMETERMAP = @{
+            Force  = @{ OriginalName = '--all'; OriginalPosition = '0'; Position = '2147483647'; ParameterType = [switch]; NoGap = $False }
+            Filter = @{ OriginalName = '--filter'; OriginalPosition = '0'; Position = '2147483647'; ParameterType = [string]; NoGap = $False }
+        }
 
-    $__outputHandlers = @{
-        Default = @{ StreamOutput = $False; Handler = { $args[0] | ConvertFrom-Json } }
-    }
-}
-PROCESS {
-    $__commandArgs = @(
-        "ps"
-        "--format"
-        "{{json .}}"
-    )
-    $__boundparms = $PSBoundParameters
-    $MyInvocation.MyCommand.Parameters.Values.Where({$_.SwitchParameter -and $_.Name -notmatch "Debug|Whatif|Confirm|Verbose" -and ! $PSBoundParameters[$_.Name]}).ForEach({$PSBoundParameters[$_.Name] = [switch]::new($false)})
-    if ($PSBoundParameters["Debug"]){wait-debugger}
-    foreach ($paramName in $PSBoundParameters.Keys|Sort-Object {$__PARAMETERMAP[$_].OriginalPosition}) {
-        $value = $PSBoundParameters[$paramName]
-        $param = $__PARAMETERMAP[$paramName]
-        if ($param) {
-            if ( $value -is [switch] ) { $__commandArgs += if ( $value.IsPresent ) { $param.OriginalName } else { $param.DefaultMissingValue } }
-            elseif ( $param.NoGap ) { $__commandArgs += "{0}""{1}""" -f $param.OriginalName, $value }
-            else { $__commandArgs += $param.OriginalName; $__commandArgs += $value |Foreach-Object {$_}}
+        $__outputHandlers = @{
+            Default = @{ StreamOutput = $False; Handler = { if ($args)
+                    {
+                        $args[0] | ConvertFrom-Json
+                    } } 
+            }
         }
     }
-    $__commandArgs = $__commandArgs|Where-Object {$_}
-    if ($PSBoundParameters["Debug"]){wait-debugger}
-    if ( $PSBoundParameters["Verbose"]) {
-         Write-Verbose -Verbose -Message docker
-         $__commandArgs | Write-Verbose -Verbose
-    }
-    $__handlerInfo = $__outputHandlers[$PSCmdlet.ParameterSetName]
-    if (! $__handlerInfo ) {
-        $__handlerInfo = $__outputHandlers["Default"] # Guaranteed to be present
-    }
-    $__handler = $__handlerInfo.Handler
-    if ( $PSCmdlet.ShouldProcess("docker")) {
-        if ( $__handlerInfo.StreamOutput ) {
-            & "docker" $__commandArgs | & $__handler
+    PROCESS
+    {
+        $__commandArgs = @(
+            "ps"
+            "--format"
+            "{{json .}}"
+        )
+        $__boundparms = $PSBoundParameters
+        $MyInvocation.MyCommand.Parameters.Values.Where( { $_.SwitchParameter -and $_.Name -notmatch "Debug|Whatif|Confirm|Verbose" -and ! $PSBoundParameters[$_.Name] }).ForEach( { $PSBoundParameters[$_.Name] = [switch]::new($false) })
+        if ($PSBoundParameters["Debug"])
+        {
+            Wait-Debugger
         }
-        else {
-            $result = & "docker" $__commandArgs
-            & $__handler $result
+        foreach ($paramName in $PSBoundParameters.Keys | Sort-Object { $__PARAMETERMAP[$_].OriginalPosition })
+        {
+            $value = $PSBoundParameters[$paramName]
+            $param = $__PARAMETERMAP[$paramName]
+            if ($param)
+            {
+                if ( $value -is [switch] )
+                {
+                    $__commandArgs += if ( $value.IsPresent )
+                    {
+                        $param.OriginalName 
+                    }
+                    else
+                    {
+                        $param.DefaultMissingValue 
+                    } 
+                }
+                elseif ( $param.NoGap )
+                {
+                    $__commandArgs += "{0}""{1}""" -f $param.OriginalName, $value 
+                }
+                else
+                {
+                    $__commandArgs += $param.OriginalName; $__commandArgs += $value | ForEach-Object { $_ }
+                }
+            }
         }
-    }
-  } # end PROCESS
+        $__commandArgs = $__commandArgs | Where-Object { $_ }
+        if ($PSBoundParameters["Debug"])
+        {
+            Wait-Debugger
+        }
+        if ( $PSBoundParameters["Verbose"])
+        {
+            Write-Verbose -Verbose -Message docker
+            $__commandArgs | Write-Verbose -Verbose
+        }
+        $__handlerInfo = $__outputHandlers[$PSCmdlet.ParameterSetName]
+        if (! $__handlerInfo )
+        {
+            $__handlerInfo = $__outputHandlers["Default"] # Guaranteed to be present
+        }
+        $__handler = $__handlerInfo.Handler
+        if ( $PSCmdlet.ShouldProcess("docker"))
+        {
+            if ( $__handlerInfo.StreamOutput )
+            {
+                & "docker" $__commandArgs | & $__handler
+            }
+            else
+            {
+                $result = & "docker" $__commandArgs
+                & $__handler $result
+            }
+        }
+    } # end PROCESS
 
-<#
+    <#
 
 
 .DESCRIPTION
@@ -73,6 +108,27 @@ The example of use the Microsoft.PowerShell.Crescendo module
 .PARAMETER Filter
 
 
+
+
+#>
+}
+
+Export-ModuleMember -Function Get-DockerContainer
+Original Command: docker ps
+
+
+.EXAMPLE
+PS> Get-DockerContainer -Force
+
+List all containers
+Original Command: docker ps -a
+
+
+.EXAMPLE
+PS> Get-DockerContainer -Force -Filter
+
+Filter output based on conditions provided
+Original Command: docker ps -a -f
 
 
 #>
--- Microsoft.PowerShell.Crescendo/script.ps1
+++ Microsoft.PowerShell.Crescendo/script.ps1
@@ -1,7 +1,5 @@
-# New-CrescendoCommand -Verb Get -Noun DockerContainer -Verbose | ConvertTo-Json -Depth 5 | Out-File example.docker.json
+# New-CrescendoCommand -Verb Get -Noun DockerContainer -Verbose | ConvertTo-Json -Depth 5 | Out-File example.docker1.json
 #  SchemaFile 'https://raw.githubusercontent.com/PowerShell/Crescendo/master/Microsoft.PowerShell.Crescendo/src/Microsoft.PowerShell.Crescendo.Schema.json'
-# https://github.com/tyconsulting/TestJsonSchema-PS
-
 Export-CrescendoModule -ConfigurationFile .\example.docker.json -ModuleName docker -Force
 
 # import module
@@ -9,7 +7,7 @@ Remove-Module -Name Docker -ErrorAction SilentlyContinue
 Import-Module .\docker.psm1
 
 # example
-Get-DockerContainer -Force -Filter name=th
+Get-DockerContainer -Force -Filter 'name=thaa'
 
 # Command      : "dotnet HotelReserva…"
 # CreatedAt    : 2020-03-19 15:52:07 +0100 CET
